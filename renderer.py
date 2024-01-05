58714afb62c7462b17a1aadca7b89b8559defe68
haoxi <haoxi@directcyber.com.au>
2024-01-06 00:26:23 +1000

    add gitlab and make ui pretty

--- renderer.py
+++ renderer.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment
 import json
 import datetime
 
 
+def get_key(kv_item):
+	d = kv_item[1]
+	if 'published' in d:
+		return d['published']
+	else:
+		return 0
+
 def render(jsonblob):
 	data = json.loads(open(jsonblob, 'r').read())
+	data_sorted = {k: v for k, v in sorted(data.items(), key=get_key, reverse=True)} 
 	# jinja2 rendering
 	environment = Environment()
 	template = environment.from_string(open("index.html.j2","r").read())
-	html = template.render(data=data, updated=datetime.datetime.utcnow().isoformat())
+	html = template.render(data=data_sorted, updated=datetime.datetime.utcnow().isoformat())
 	with open('index.html', 'w+') as f:
 		f.write(html)
 	print('rendered to index.html')
